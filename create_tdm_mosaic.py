27bc2227378363e17e4aa47b678971d3ab752c84
Amaury Dehecq <amaury.dehecq@jpl.nasa.gov>
2020-08-03 05:50:52 -0700

    Important changes and bug fixes to: - make the geoid correction optional - allow creating the mosaic in a different projection system. This is preferred since in lat/lon, different tiles may have different grid cell sizes - add more comments

--- create_tdm_mosaic.py
+++ create_tdm_mosaic.py
@@ -13,13 +13,15 @@ from glob import glob
 import numpy as np
 import os, argparse
 from subprocess import call, check_call
+import osr, ogr
+import shlex
 
 # Own libraries
 import georaster as geor
 
 
 ## Set up arguments ##
-parser = argparse.ArgumentParser(description='Create a mosaic TDX DEM from tiles stored locally. The tiles are merged, making sure that no horizontal shift is introduced, filtered by removing HEM > 0.5 pixels and setting the geoid to EGM96.')
+parser = argparse.ArgumentParser(description='Create a mosaic TDX DEM from tiles stored locally. The tiles are merged, making sure that no horizontal shift is introduced, filtered by removing HEM > 0.5 pixels and optionally setting the geoid to EGM96.')
 
 # Positional arguments
 parser.add_argument('dataDir', type=str, help='str, path to the folder containing all DEM files. The tree below contains [NS]??/[EW]???/TDM1_DEM__30_[NS]??[EW]???_V01_C/DEM/TDM1_DEM__30_[NS]??[EW]???_DEM.tif and [NS]??/[EW]???/TDM1_DEM__30_[NS]??[EW]???_V01_C/AUXFILES/TDM1_DEM__30_[NS]??[EW]???_HEM.tif.')
@@ -27,7 +29,12 @@ parser.add_argument('outfile', type=str, help='str, path to the output raster fi
     
 # Optional arguments
 parser.add_argument('-te', dest='extent', type=float, help='output DEM extent xmin ymin xmax ymax',nargs=4)
-parser.add_argument('-r', dest='resampling', type=str, default='bilinear', help='resampling algorithm to use, check GDAL for a list of algorithms available (Default is bilibear)')
+parser.add_argument('-tr', dest='tr', type=float, default=None, help='float, the output DEM resolution in t_srs units. Default is determined automatically by gdal from original tiles. WARNING: this can lead to strong resampling for high latitudes as TDX tiles are not square, but gdal assumes square tiles.')
+parser.add_argument('-t_srs', dest='srs', type=str, default=None, help='str, a PROJ-4 string of the output projection. Default is same as input tiles, i.e. EPSG 4326.')
+parser.add_argument('-r', dest='resampling', type=str, default='cubic', help='resampling algorithm to use, check GDAL for a list of algorithms available (Default is cubic)')
+parser.add_argument('-ot', dest='ot', type=str, default='Int16', help='output data type. Original is Float32 but default is saved as Int16 to save space.')
+parser.add_argument('-geoid', dest='geoid', action='store_true', help='if set, will express elevation with reference to the EGM96 geoid. TDX DEM is w.r.t. the WGS84 ellipsoid.')
+parser.add_argument('-co', dest='co', type=str, default=['COMPRESS=LZW','TILED=YES','BLOCKXSIZE=256','BLOCKYSIZE=256','BIGTIFF=IF_SAFER'], help="GDAL creation options (Default is COMPRESS=LZW TILED=YES BLOCKXSIZE=256 BLOCKYSIZE=256 BIGTIFF=IF_SAFER", nargs='*')
 parser.add_argument('-overwrite', dest='overwrite', action='store_true', help='if set, will overwrite output file')
     
 args = parser.parse_args()
@@ -50,11 +57,12 @@ if args.extent is None:
     raise SystemExit('ERROR: Extent must be specified')
 
 # Check that ASP is loaded
-try:
-    FNULL = open(os.devnull, 'w')
-    call('dem_geoid', stdout=FNULL)
-except FileNotFoundError:
-    raise SystemExit("ERROR: ASP's dem_geoid must be in your path")
+if args.geoid:
+    try:
+        FNULL = open(os.devnull, 'w')
+        call('dem_geoid', stdout=FNULL)
+    except FileNotFoundError:
+        raise SystemExit("ERROR: ASP's dem_geoid must be in your path")
 
 
 # Find all DEM tiles
@@ -94,18 +102,47 @@ for f in DEMfiles:
 lons = np.array(lons)
 lats = np.array(lats)
 
-# Select tiles overlapping with requested extent
+## Select tiles overlapping with requested extent ##
+# This fails for features extending over the 180 degree line. Would need to read each tile extent, convert to output SRS and test overlap instead.
 
 xmin, ymin, xmax, ymax = args.extent
 
+if args.srs is not None:
+    # Create an SRS object for output proj
+    out_srs = osr.SpatialReference()
+    out_srs.ImportFromProj4(args.srs)
+
+    # Try to get EPSG code
+    out_srs.AutoIdentifyEPSG()
+    epsg = out_srs.GetAuthorityCode(None) 
+
+    # If 4326, then nothing to be done, otherwise, need to convert to lat/lon
+    if epsg=='4326':
+        lonmin, latmin, lonmax, latmax = args.extent
+    else:
+        epsg4326 = osr.SpatialReference()
+        epsg4326.ImportFromEPSG(4326)
+        epsg4326.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)   # Since GDAL 3.0, needed to make sure that WGS84 uses long/lat rather than the opposite
+        tr = osr.CoordinateTransformation(out_srs, epsg4326)
+        wkt = "POLYGON ((%f %f, %f %f, %f %f, %f %f, %f %f))" \
+            %(xmin,ymin,xmin,ymax,xmax,ymax,xmax,ymin,xmin,ymin)
+        poly = ogr.CreateGeometryFromWkt(wkt)
+        poly.AssignSpatialReference(epsg4326)
+        poly.Transform(tr)
+        lonmin, lonmax, latmin, latmax = poly.GetEnvelope()
+        print("Corresponding extent (lonmin, lonmax, latmin, latmax): %g, %g, %g, %g" %(lonmin,lonmax,latmin,latmax))
+else:
+    lonmin, latmin, lonmax, latmax = args.extent
+
+# Select tiles
 tiles2keep = []
 for k in range(len(DEMfiles)):
-    if (lons[k] >= xmin) & (lons[k] <= xmax+2):  #+2 because some tiles are 2 degrees wide
-        if (lats[k] >= ymin) & (lats[k] < ymax):
+    if (lons[k] >= lonmin-1) & (lons[k] <= lonmax):
+        if (lats[k] >= latmin-1) & (lats[k] <= latmax):
             tiles2keep.append(k)
 
 DEMfiles = DEMfiles[tiles2keep]
-
+print("Found %i tiles overlapping with set extent" %len(DEMfiles))
 
 ## First mask pixels with HEM > 0.5 in TDX DEM ##
 
@@ -142,24 +179,40 @@ np.savetxt(listfile,masked_DEM_files,fmt='%s')
 
 
 # Create merged VRT file
+# if no resampling method specified, can lead to N/S oriented banding artifacts when tiles above 70N (with different grid cell) are included
 print("* Merge tiles *")
 vrtfile = outDir + 'merged_DEM.vrt'
 cmd = "gdalbuildvrt -r %s %s -input_file_list %s" %(args.resampling, vrtfile, listfile)
 cprint(cmd); check_call(cmd.split())
 
-# Crop mosaic to final extent
+## Create final mosaic ##
 # Must use gdalwarp, as option -te in gdalbuildvrt creates horizontal shift...
-print("* Crop DEM *")
+print("* Create final mosaic *")
+
 mosaic_file = outDir + 'merged_dem.tif'
-cmd = "gdalwarp -te %g %g %g %g -r %s %s %s" %(xmin, ymin, xmax, ymax, args.resampling, vrtfile, mosaic_file)
-cprint(cmd); check_call(cmd.split())
+
+# gdalwarp options
+gdal_opts = ''
+if args.tr is not None:
+    gdal_opts += " -tr %g %g" %(args.tr, args.tr)
+if args.srs is not None:
+    gdal_opts += " -t_srs '%s'" %args.srs
+if len(args.co)>0:
+    gdal_opts += ' -co ' + ' -co '.join(args.co)
+
+# gdalwarp command
+cmd = "gdalwarp -te %f %f %f %f -r %s %s %s -ot %s %s" %(xmin, ymin, xmax, ymax, args.resampling, vrtfile, mosaic_file, args.ot, gdal_opts)
+cprint(cmd); check_call(shlex.split(cmd))  # shlex.split ignore spaces in-between quotes, e.g. in t_srs
 
 # Correct geoid
-print("* Convert to geoid EGM96 *")
-outPrefix = outDir + 'merged_DEM'
-corrDEMf = outPrefix + '-adj.tif'
-cmd = "dem_geoid --geoid EGM96 %s -o %s" %(mosaic_file,outPrefix)
-cprint(cmd); check_call(cmd.split())
+if args.geoid:
+    print("* Convert to geoid EGM96 *")
+    outPrefix = outDir + 'merged_DEM'
+    corrDEMf = outPrefix + '-adj.tif'
+    cmd = "dem_geoid --geoid EGM96 %s -o %s" %(mosaic_file,outPrefix)
+    cprint(cmd); check_call(cmd.split())
+else:
+    corrDEMf = mosaic_file
 
 # Mv to final DEM
 print("* Move and delete temporary folder *")
